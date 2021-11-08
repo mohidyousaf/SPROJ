a7b324d0bd7841c3b42dcc1d44ee9553f386725d
ayantaby1 <ayanster103@gmail.com>
2021-11-08 20:39:48 +0500

    Database

---
+++ Project 09/Prototype/petswala/lib/models/PetMerchandiseShops.dart
@@ -0,0 +1,9 @@
+class PetMerchandiseShops {
+
+  final String shopname;
+  final int shopcontact;
+  final String shopaddress;
+
+  PetMerchandiseShops({ required this.shopname, required this.shopcontact,required this.shopaddress });
+
+}
